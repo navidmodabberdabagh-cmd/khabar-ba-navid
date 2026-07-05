6ae8841cf084dced832a086d03c64eb6730cecff
navidmodabberdabagh-cmd <navidmodabberdabagh@gmail.com>
2026-07-05 18:54:52 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+RSS_FEEDS = {
+    "BBC فارسی": "https://feeds.bbci.co.uk/persian/rss.xml",
+    "یورونیوز فارسی": "https://parsi.euronews.com/rss",
+    "العربیه فارسی": "https://farsi.alarabiya.net/rss.xml",
+    "Al Jazeera English": "https://www.aljazeera.com/xml/rss/all.xml",
+    "Daily Sabah (Turkey)": "https://www.dailysabah.com/rssFeed/10000",
+    "Hürriyet (Turkey)": "https://www.hurriyet.com.tr/rss/anasayfa",
+    "Times of Israel": "https://www.timesofisrael.com/feed/",
+    "Jerusalem Post": "https://www.jpost.com/rss/rssfeedsfrontpage.aspx",
+}
 
+KEYWORDS = [
+    "iran", "ایران", "tehran", "تهران", "trump", "ترامپ",
+    "hezbollah", "حزب الله", "israel", "اسرائیل",
+    "iraq", "عراق", "pakistan", "پاکستان", "afghanistan", "افغانستان",
+    "azerbaijan", "آذربایجان", "armenia", "ارمنستان", "russia", "روسیه",
+    "ukraine", "اوکراین", "greece", "یونان", "turkey", "ترکیه",
+    "persian gulf", "خلیج فارس", "uae", "امارات", "saudi", "عربستان",
+    "qatar", "قطر", "kuwait", "کویت", "bahrain", "بحرین", "oman", "عمان",
+    "iran football", "فوتبال ایران", "iran economy", "اقتصاد ایران",
+    "iran sanctions", "تحریم ایران", "migration", "مهاجرت",
+    "رضا پهلوی", "pahlavi",
+]

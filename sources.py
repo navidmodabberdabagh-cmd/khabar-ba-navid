600390fb24e3d7589b001053ec51cc693416ffdd
navidmodabberdabagh-cmd <navidmodabberdabagh@gmail.com>
2026-07-07 16:28:29 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 RSS_FEEDS = {
+    "Fox News": "https://moxie.foxnews.com/google-publisher/latest.xml",
+    "Fox News World": "https://moxie.foxnews.com/google-publisher/world.xml",
+    "DW English": "https://rss.dw.com/rdf/rss-en-all",
+    "ایران اینترنشنال": "https://www.iranintl.com/fa/rss",
+    "CNBC": "https://www.cnbc.com/id/100003114/device/rss/rss.html",
+    "France24": "https://www.france24.com/en/rss",
+    "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
     "ایسنا": "https://www.isna.ir/rss",
     "ایرنا": "https://www.irna.ir/rss",
-    "ایران اینترنشنال": "https://www.iranintl.com/fa/rss",
     "یورونیوز فارسی": "https://parsi.euronews.com/rss",
     "العربیه فارسی": "https://farsi.alarabiya.net/rss.xml",
-    "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
     "Al Arabiya": "https://english.alarabiya.net/tools/rss",
     "Arab News": "https://www.arabnews.com/rss.xml",
     "Gulf News": "https://gulfnews.com/rss",
@@ -15,13 +20,10 @@ RSS_FEEDS = {
     "Daily Sabah": "https://www.dailysabah.com/rssFeed/10000",
     "Hürriyet": "https://www.hurriyet.com.tr/rss/anasayfa",
     "Anadolu Agency": "https://www.aa.com.tr/en/rss/default?cat=live",
-    "France24": "https://www.france24.com/en/rss",
-    "DW English": "https://rss.dw.com/rdf/rss-en-all",
     "RFI": "https://www.rfi.fr/en/rss",
     "AP News": "https://apnews.com/rss",
     "Reuters": "https://www.reutersagency.com/feed/?best-topics=middle-east",
     "CBC": "https://www.cbc.ca/cmlink/rss-world",
-    "MercoPress": "https://en.mercopress.com/rss/",
     "News24": "https://www.news24.com/rss",
     "Nikkei Asia": "https://asia.nikkei.com/rss/feed/nar",
     "South China Morning Post": "https://www.scmp.com/rss/91/feed",
@@ -29,8 +31,6 @@ RSS_FEEDS = {
     "Science Daily": "https://www.sciencedaily.com/rss/health_medicine.xml",
     "Medical Xpress": "https://medicalxpress.com/rss-feed/",
     "BBC Sport Football": "https://feeds.bbci.co.uk/sport/football/rss.xml",
-    "Fox News": "https://moxie.foxnews.com/google-publisher/latest.xml",
-    "Fox News World": "https://moxie.foxnews.com/google-publisher/world.xml",
     "EU Home Affairs": "https://home-affairs.ec.europa.eu/rss_en",
     "GOV UK Home Office": "https://www.gov.uk/government/organisations/home-office.atom",
 }
@@ -40,20 +40,34 @@ KEYWORDS = [
     "hezbollah", "حزب الله", "israel", "اسرائیل",
     "iraq", "عراق", "pakistan", "پاکستان", "afghanistan", "افغانستان",
     "azerbaijan", "آذربایجان", "armenia", "ارمنستان", "russia", "روسیه",
-    "ukraine", "اوکراین", "greece", "یونان", "turkey", "ترکیه",
+    "ukraine", "اوکراین", "اکراین", "greece", "یونان", "turkey", "ترکیه",
     "persian gulf", "خلیج فارس", "uae", "امارات", "saudi", "عربستان",
     "qatar", "قطر", "kuwait", "کویت", "bahrain", "بحرین", "oman", "عمان",
-    "iran football", "فوتبال ایران", "iran economy", "اقتصاد ایران",
+    "china", "چین", "iran football", "فوتبال ایران", "iran economy", "اقتصاد ایران",
     "iran sanctions", "تحریم ایران", "migration", "مهاجرت",
     "رضا پهلوی", "pahlavi",
     "بورس", "stock market", "oil price", "نفت برنت", "borsa",
     "cancer treatment", "درمان سرطان", "cancer cure", "immunotherapy",
     "desalination", "آب شیرین‌سازی", "water technology",
-    "world cup", "جام جهانی", "gulf cup", "لیگ خلیج فارس", "team melli",
-    "istanbul weather", "هوای استانبول", "بارش شدید", "هشدار زرد",
-    "heatwave", "موج گرما", "cold wave", "موج سرما", "heavy snow", "برف سنگین",
-    "flood warning", "هشدار سیل", "تبریز", "شیراز", "اصفهان", "کرمان",
-    "بندرعباس", "کردستان", "گیلان", "مازندران",
+    "world cup", "جام جهانی", "gulf cup", "لیگ خلیج فارس", "team melli", "football", "فوتبال",
     "visa", "ویزا", "residence permit", "اقامت", "immigration law", "قانون مهاجرت",
     "asylum", "پناهندگی", "deportation", "اخراج مهاجران",
+    # آب و هوا فقط با این کلمات + شهر مجاز match می‌شود (فیلتر جداگانه در bot.py)
+    "weather", "storm", "flood", "snow", "rain", "tsunami",
 ]
+
+# فقط این شهرها/استان‌ها برای اخبار آب‌وهوا مجازند
+WEATHER_ALLOWED_LOCATIONS = [
+    "استانبول", "آنتالیا", "تهران", "آذربایجان شرقی", "آذربایجان غربی",
+    "اصفهان", "خراسان رضوی", "مشهد", "فارس", "شیراز", "گیلان", "مازندران",
+    "آبادان", "هرمزگان", "کیش", "بوشهر", "یزد", "کرمان",
+    "سیستان و بلوچستان", "زاهدان", "چابهار",
+]
+
+WEATHER_SEVERITY_WORDS = [
+    "باران شدید", "بارش شدید", "برف سنگین", "سیل", "سیلاب", "سونامی",
+    "هشدار قرمز", "هشدار زرد", "طوفان", "heavy rain", "heavy snow",
+    "flood", "tsunami", "storm warning",
+]
+
+MEDICAL_SOURCES = {"Science Daily", "Medical Xpress"}
